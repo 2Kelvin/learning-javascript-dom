f4f55ace473b25da4513d8e69fa035a7520f4fea
2Kelvin <kvin.mwangi254@gmail.com>
2022-10-02 11:36:17 +0300

    created & removed attributes thro Javascript

--- creating-dom-elements/dom.js
+++ creating-dom-elements/dom.js
@@ -2,4 +2,36 @@ const ul = document.querySelector("ul");
 const li = document.createElement("li");
 
 ul.append(li);
-console.log(ul);
+
+// creating & modifying text --> innerText, innerHTML, textContent
+const firstListItem = document.querySelector("li");
+
+// innerText returns JUST the text between the HTML tags
+console.log(firstListItem.innerText);
+
+// textContent returns the raw text as it is in the HTML page, with indents and everything if any
+console.log(firstListItem.textContent);
+
+// innerHTML returns the raw text + any HTML elements inside the specified element
+console.log(firstListItem.innerHTML);
+
+li.innerText = "GitHub Projects";
+
+
+// CREATING & REMOVING ATTRIBUTES & CLASSES THROUGH JAVASCRIPT
+li.setAttribute("class", "listStyle"); //set this class from the linked CSS stylesheet to this element thro Javascript
+
+// this is how you remove an attribute from an element thro Javascript
+// li.removeAttribute("class");
+
+// this removes the element itself from the DOM
+// li.remove();
+
+// ACCESSING ATTRIBUTES THROUGH JAVASCRIPT
+const mainHeading = document.getElementById("mainHeading");
+console.log(mainHeading.getAttribute("id"));
+
+// viewing all attribute names being used by this element
+console.log(mainHeading.getAttributeNames());
+
+// CSS CLASSES
